99b06861a439bdf0d260c97f3a986d397d51db05
sharaddhapatil1996 <patilshraddha5696@gmail.com>
2024-07-20 20:23:12 +0530

    Second commit by shraddha

---
+++ GitPractice/FibonacciSeries.java
@@ -0,0 +1,22 @@
+package GitPractice;        //input =0,1,1,2,3,5,8,13,21,34,55 etc
+
+public class FibonacciSeries
+{
+    public static void main(String[] args)
+    {
+        int num1=0, num2=1,num=10;
+
+        for(int i=0;i<=10;i++)
+        {
+            System.out.print(num1+" ");
+            int num3=num2+num1;
+            num1=num2;
+            num2=num3;
+
+        }
+
+
+
+
+    }
+}
